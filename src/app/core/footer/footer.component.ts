a01c508ab8d426d36948e796b3acb40724175904
Ayush Dhimmar <ayush.dhimmar@1rivet.local>
2023-03-03 18:07:32 +0530

    feat: overlay

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  constructor(private modalService: NgbModal) {
+
+  }
+
+  openVerticallyCentered(content: any) {
+    this.modalService.open(content, { centered: true });
+  }
 }
